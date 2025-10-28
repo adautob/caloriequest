78038c2d8aaada87a3c969102017246e139ab21c
Adauto Bueno <adauto.bueno@gmail.com>
2025-10-28 23:21:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-930682958-d2971",
+  "appId": "1:1056847295687:web:f485847273098a0509fae2",
+  "apiKey": "AIzaSyAcZOm5-y72PAxpAASOWZGjCLlNj5JKfZg",
+  "authDomain": "studio-930682958-d2971.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1056847295687"
+};
